b1d5753980aa5add209f26cfe4c45be457cc8871
nalbarr <nalbarracin@gmail.com>
2020-09-06 17:45:56 +0000

    NAA. Modify 4_psql_connect.sh to pull from env/set_env.sh.

--- 4_psql_connect.sh
+++ 4_psql_connect.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 
-#DB_NAME=test
-#USER_NAME=postgres
-#export PGPASSWORD=postgres123@
-
-DB_NAME=postgres
-USER_NAME=postgres
-
-psql "host=127.0.0.1 sslmode=disable dbname=$DB_NAME user=$USER_NAME"
+psql "host=127.0.0.1 sslmode=disable dbname=$SQL_DATABASE user=$PGUSER"
 
